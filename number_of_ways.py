0bcb4d2c0e33f24885cd47e2bfe15ed00084e9c3
Sanjay Kumar <sanjay81k2@gmail.com>
2023-01-05 06:20:15 +0530

    Add environment.yml and cosmetically changed number_of_ways.py
    This is just a test commit with no consequences.

--- number_of_ways.py
+++ number_of_ways.py
@@ -1,6 +1,5 @@
 from tqdm import tqdm
 
-
 def numberOfWays(startPos: int, endPos: int, k: int) -> int:
     """
     Solving Leetcode Problem.
@@ -22,7 +21,7 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
     for i in tqdm(range(k)):
         for path in paths:
             new_path = path.copy()
-            last_position = new_path[-1]
+            last_position = new_path[-1] 
 
             # exist fast if not going to make to end
             if endPos - last_position > (k - i - 1):
